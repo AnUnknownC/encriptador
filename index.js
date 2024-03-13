75c770ed2127d1c7d654e7cfaae5f0e8afe400e2
AnUnknownC <danesc401@hotmail.com>
2024-03-13 18:56:06 -0500

    Finished encrypt/decrypt functions

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-// Claves para la encriptacion
-// La letra "e" es convertida para "enter"
-// La letra "i" es convertida para "imes"
-// La letra "a" es convertida para "ai"
-// La letra "o" es convertida para "ober"
-// La letra "u" es convertida para "ufat"
-
 // Mensaje Secreto
 // fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!
 
+clavesEncriptacion = {
+    "e" : "enter",
+    "i" : "imes",
+    "a" : "ai",
+    "o" : "ober",
+    "u" : "ufat"
+}
 
-function desencriptar(){
-    alert("Buenas")
+function assignElementText(element, text){
+    var elementHTML = document.querySelector(element);
+    elementHTML.innerHTML = text;
 }
 
 function encriptar(){
-    alert("tardes")
+    palabra = document.getElementById('inputTexto').value
+    let traduccion = ''
+    for (var i = 0; i < palabra.length; i++){
+        if (palabra[i] in clavesEncriptacion){
+            traduccion += clavesEncriptacion[palabra[i]]
+        }
+        else{
+            traduccion += palabra[i]
+        }
+    }
+    assignElementText('div', traduccion)
+}
+
+function desencriptar(){
+    palabra = document.getElementById('inputTexto').value
+    let comp = ''
+    let traduccion = ''
+    for (var i = 0; i < palabra.length; i++){
+        if (palabra[i] in clavesEncriptacion){
+            traduccion += palabra[i]
+            i += clavesEncriptacion[palabra[i]].length-1
+        }
+        else{
+            traduccion += palabra[i]
+        }
+    }
+    assignElementText('div', traduccion)
 }

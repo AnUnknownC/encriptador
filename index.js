a8a3cf9cee3311ff775e5c0c5d99081f0bc46d5d
AnUnknownC <danesc401@hotmail.com>
2024-03-12 22:46:03 -0500

    Added the encriptor keys and the text to unecrypt to the js file

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// Claves para la encriptacion
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+// Mensaje Secreto
+// fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!
+
+
 function desencriptar(){
     alert("Buenas")
 }

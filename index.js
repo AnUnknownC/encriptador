354787f51356817fe1586084b770b19b63c693b6
AnUnknownC <88901481+AnUnknownC@users.noreply.github.com>
2024-03-19 20:51:24 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -9,37 +9,51 @@ clavesEncriptacion = {
     "u" : "ufat"
 }
 
+button = document.getElementById("copiarTexto")
+
+
 function assignElementText(element, text){
     var elementHTML = document.querySelector(element);
     elementHTML.innerHTML = text;
 }
 
 function encriptar(){
-    palabra = document.getElementById('inputTexto').value
-    let traduccion = ''
+    palabra = document.getElementById('inputTexto').value;
+    let traduccion = '';
     for (var i = 0; i < palabra.length; i++){
         if (palabra[i] in clavesEncriptacion){
-            traduccion += clavesEncriptacion[palabra[i]]
+            traduccion += clavesEncriptacion[palabra[i]];
         }
         else{
-            traduccion += palabra[i]
-        }
+            traduccion += palabra[i];
+        };
     }
-    assignElementText('div', traduccion)
+    assignElementText('div', traduccion);
 }
 
 function desencriptar(){
-    palabra = document.getElementById('inputTexto').value
-    let comp = ''
-    let traduccion = ''
+    palabra = document.getElementById('inputTexto').value;
+    let traduccion = '';
     for (var i = 0; i < palabra.length; i++){
         if (palabra[i] in clavesEncriptacion){
-            traduccion += palabra[i]
-            i += clavesEncriptacion[palabra[i]].length-1
+            traduccion += palabra[i];
+            i += clavesEncriptacion[palabra[i]].length-1;
         }
         else{
-            traduccion += palabra[i]
-        }
-    }
-    assignElementText('div', traduccion)
+            traduccion += palabra[i];
+        };
+    };
+    assignElementText('div', traduccion);
+    return traduccion;
 }
+
+
+button.addEventListener("click", function () {
+    var textoACopiar = document.getElementById("procesoRealizado").innerText;
+    var elementoTemporal = document.createElement("textarea");
+    elementoTemporal.value = textoACopiar;
+    document.body.appendChild(elementoTemporal);
+    elementoTemporal.select();
+    document.execCommand("copy");
+    document.body.removeChild(elementoTemporal);
+})
